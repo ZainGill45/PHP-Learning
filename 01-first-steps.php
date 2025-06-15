fe104609fa5320f2e2b6bae8904a62ba39e69680
Zain Gill <zaingill45@gmail.com>
2025-06-14 18:27:05 -0600

    update

--- 01-first-steps.php
+++ 01-first-steps.php
@@ -5,10 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            background-color: #333333;
+            text-align: center;
+            color: #f4f4f4;
+        }
+    </style>
 </head>
 
 <body>
     <h1>Hey, this is my first PHP file</h1>
+    <h2>
+        <?php
+            $randomD20Number = rand(min: 1, max: 20);
+
+            echo "Your D20 rolled: $randomD20Number";
+        ?>
+    </h2>
 </body>
 
 </html>
